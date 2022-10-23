caacc36e4dc5d3d2b6dbde9854576ddd4aa108d0
aliifam <aliif.arief.m@gmail.com>
2022-10-23 08:40:12 +0700

    feat: major refactoring adding pagination & change ssearching method

---
+++ assets/js/cari.js
@@ -0,0 +1,41 @@
+var list = document.getElementById("list");
+var awal = list.innerHTML;
+var query = document.getElementById("search-input");
+
+var allpost;
+query.onfocus = function fetchpost() {
+    fetch("/index.json")
+        .then((res) => res.json())
+        .then((data) => {
+            allpost = data;
+        });
+};
+
+query.oninput = function searchpost() {
+    list.innerHTML = "";
+    for (const artikel of allpost) {
+        if (
+            artikel.title
+                .toLocaleLowerCase()
+                .trim()
+                .includes(
+                    query.value.toLocaleLowerCase().trim() && query.value.trim()
+                )
+        ) {
+            list.innerHTML += `
+                <article class="list">
+                <a href="${artikel.url}">${artikel.title}</a>
+                <time>${artikel.date}</time>
+            </article>`;
+        }
+    }
+    if (!list.innerHTML) {
+        list.innerHTML = `<p style="font-size: large;font-weight: 600;">Sorry nothing article matched with "${query.value}"</p>`;
+    }
+};
+
+query.onblur = function gatau() {
+    if (!query.value.trim()) {
+        list.innerHTML = awal;
+    }
+};
