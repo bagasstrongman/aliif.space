a22ad4914d05fc1bfe39aac7210335e984290fbc
aliifam <aliif.arief.m@gmail.com>
2022-11-01 09:54:43 +0700

    fixing: double hit

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,16 +1,22 @@
-fetch(`https://api.countapi.xyz/hit/${location.hostname}${location.pathname}`)
-    .then((res) => res.json())
-    .then((data) => {
-        if (parseInt(data.value) > 1) {
-            document.getElementById(
-                "stats"
-            ).innerText = ` · ${data.value} views`;
-        } else {
-            document.getElementById(
-                "stats"
-            ).innerText = ` · ${data.value} view`;
-        }
-    })
-    .catch((e) => {
-        console.log(e);
-    });
+document.addEventListener("rasaspa:load", function () {
+    if (document.getElementById("stats")) {
+        fetch(
+            `https://api.countapi.xyz/hit/${location.hostname}${location.pathname}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                if (parseInt(data.value) > 1) {
+                    document.getElementById(
+                        "stats"
+                    ).innerText = ` · ${data.value} views`;
+                } else {
+                    document.getElementById(
+                        "stats"
+                    ).innerText = ` · ${data.value} view`;
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+});
